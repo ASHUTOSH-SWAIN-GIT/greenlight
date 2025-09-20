fa83e34d5de7ee734727b5712e6314a2ae8b3182
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-09-21 04:40:37 +0530

    established db connection

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module greenlight.ashutosh.net
 
 go 1.25.1
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require (
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.9
+)
--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -8,25 +8,25 @@ var (
 	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 )
 
-type validator struct {
+type Validator struct {
 	Errors map[string]string
 }
 
-func New() *validator {
-	return &validator{Errors: make(map[string]string)}
+func New() *Validator {
+	return &Validator{Errors: make(map[string]string)}
 }
 
-func (v *validator) Valid() bool {
+func (v *Validator) Valid() bool {
 	return len(v.Errors) == 0
 }
 
-func (v *validator) AddError(key, message string) {
+func (v *Validator) AddError(key, message string) {
 	if _, exists := v.Errors[key]; !exists {
 		v.Errors[key] = message
 	}
 }
 
-func (v *validator) Check(ok bool, key, message string) {
+func (v *Validator) Check(ok bool, key, message string) {
 	if !ok {
 		v.AddError(key, message)
 	}
