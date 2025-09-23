1d21730df09b9ab2503b657dc25ee40c9c3a42bb
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-09-23 09:01:07 +0530

    delete functionality

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -86,7 +86,26 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 }
 
 func (m MovieModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+	query := `
+		DELETE FROM movies 
+		WHERE id =  $1
+	`
+	result, err := m.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
 	return nil
+
 }
 
 func (m MovieModel) Update(movie *Movie) error {
